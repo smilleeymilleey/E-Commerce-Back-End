531417a860b70edd5edb8012b97243ca4911d1de
Caitlyn <caitlynmiley.cm@gmail.com>
2021-04-25 16:10:36 -0500

    added a put request to update id of a category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,47 +1,46 @@
 const router = require('express').Router();
-
 const { Category, Product } = require('../../models');
-
-
 
 router.get('/', async (req, res) => {
   try {
     const siteCategories = await Category.findAll()
     console.log("from api/")
      res.status(200).json(siteCategories)
-  }catch (error) {
+  } catch (error) {
     res.status(404).json(error);
   }
   });
  
-
-
 router.get('/:id', async (req, res) => {
-try {
-  let id = req.params.id
-   const categoryPK = await Product.findAll({
-     where: {
-      category_id: id
-   }
-  });
-  console.log("from category/:id")
-    res.status(200).json(categoryPK)
-}catch (error) {
-  res.status(404).json(error)
-}
- 
+  try {
+    let id = req.params.id
+    const categoryPK = await Product.findAll({
+      where: {
+        category_id: id
+        }
+      });
+      console.log("from category/:id")
+      res.status(200).json(categoryPK)
+  } catch (error) {
+    res.status(404).json(error)
+  }
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
   let newCategory = await Category.create({
     category_name: req.body.category_name
   })
   res.status(200).json(newCategory)
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  let newId = await Category.update({category_name: req.body.category_name}, {
+    where: {
+      id: req.params.id
+    }
+  })
+  res.status(200).json(newId)
+ 
 });
 
 router.delete('/:id', (req, res) => {
